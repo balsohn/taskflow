0bfe3bf9972b8e858ea6d9f53adcef10b87c5157
KimDohwan24 <94787456+KimDohwan24@users.noreply.github.com>
2025-06-16 21:25:18 +0900

    feat : User create & Jwt & Security

--- src/main/java/com/example/taskflow/domain/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -1,13 +1,30 @@
 package com.example.taskflow.domain.user.controller;
 
+import com.example.taskflow.domain.user.dto.UserRequestDto;
+import com.example.taskflow.domain.user.dto.UserResponseDto;
 import com.example.taskflow.domain.user.service.UserService;
+import com.example.taskflow.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> signup(@RequestBody UserRequestDto userRequestDto){
+
+        ApiResponse signupDto = userService.signup(userRequestDto);
+
+        return new ResponseEntity<>(signupDto, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/example/taskflow/domain/user/service/UserService.java
+++ src/main/java/com/example/taskflow/domain/user/service/UserService.java
@@ -1,13 +1,38 @@
 package com.example.taskflow.domain.user.service;
 
+import com.example.taskflow.domain.user.dto.UserRequestDto;
+import com.example.taskflow.domain.user.dto.UserResponseDto;
+import com.example.taskflow.domain.user.entity.User;
 import com.example.taskflow.domain.user.repository.UserRepository;
+import com.example.taskflow.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
 
+    // 유저 생성
+    public ApiResponse signup(UserRequestDto userRequestDto) {
+
+        // TODO : 비밀번호 암호화 필요
+        log.info(userRequestDto.getUsername());
+        log.info(userRequestDto.getEmail());
+        log.info(userRequestDto.getName());
+        log.info(userRequestDto.getPassword());
+
+        User user = new User(userRequestDto.getUsername(), userRequestDto.getName(), userRequestDto.getPassword(), userRequestDto.getEmail());
+
+
+
+        User saveUser = userRepository.save(user);
+
+        return new ApiResponse<>(true, "sert",userRequestDto);
+    }
 }
