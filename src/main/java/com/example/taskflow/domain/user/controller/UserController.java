4267526d7cc28c0aaa53220296e7351aafb9b758
KimDohwan24 <94787456+KimDohwan24@users.noreply.github.com>
2025-06-16 12:23:44 +0900

    refactor : 유저 기초 세팅

---
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.taskflow.domain.user.controller;
+
+import com.example.taskflow.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+}
---
+++ src/main/java/com/example/taskflow/domain/user/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.taskflow.domain.user.entity;
+
+import com.example.taskflow.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+@Getter
+@Setter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 로그인할때 쓸 아이디
+    @NotBlank
+    private String username;
+
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String password;
+
+    @Email
+    @Column(unique = true)
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "유효한 이메일 형식이 아닙니다."
+    )
+    private String email;
+
+    @Column
+    private String role;
+}
+
