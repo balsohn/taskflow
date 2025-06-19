21285e38aeda85a7ee37df22c53a5e205033ce76
user <xkrhd3@naver.com>
2025-06-19 17:19:27 +0900

    docs: 댓글 삭제 구현

--- src/main/java/com/example/taskflow/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/taskflow/domain/comment/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.example.taskflow.domain.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Page<Comment> findAllByTaskId(Long taskId, Pageable pageable);
+    Page<Comment> findByid(Long id,Pageable pageable);
 
 }
